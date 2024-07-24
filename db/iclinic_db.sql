e490d2b676cd377cbe285af90c7ab5ce6baf2f19
itsmeralph09 <ralphenigmatic@gmail.com>
2024-07-24 20:08:43 +0800

    Added updated db file

--- db/iclinic_db.sql
+++ db/iclinic_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jul 13, 2024 at 07:50 AM
+-- Generation Time: Jul 24, 2024 at 02:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,39 @@ SET time_zone = "+00:00";
 --
 -- Database: `iclinic_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_tbl`
+--
+
+CREATE TABLE `admin_tbl` (
+  `admin_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `middle_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `suffix_name` varchar(20) NOT NULL,
+  `birthdate` date NOT NULL,
+  `age` int(10) NOT NULL,
+  `sex` varchar(20) NOT NULL,
+  `contact_no` varchar(12) NOT NULL,
+  `personal_address` varchar(100) NOT NULL,
+  `occupation` varchar(100) NOT NULL,
+  `emergency_contact_name` varchar(100) NOT NULL,
+  `emergency_contact_no` varchar(12) NOT NULL,
+  `emergency_contact_address` varchar(100) NOT NULL,
+  `profile` varchar(255) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0 COMMENT '1 = deleted, 0 = not deleted'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_tbl`
+--
+
+INSERT INTO `admin_tbl` (`admin_id`, `user_id`, `first_name`, `middle_name`, `last_name`, `suffix_name`, `birthdate`, `age`, `sex`, `contact_no`, `personal_address`, `occupation`, `emergency_contact_name`, `emergency_contact_no`, `emergency_contact_address`, `profile`, `deleted`) VALUES
+(4, 15, 'FRIEDA', 'TREVOR', 'REISS', 'NA', '2001-05-09', 23, 'FEMALE', '09111111111', 'ILANG ILANG STREET', 'STAFF', 'RALPH CUSTODIO', '09222222222', 'PUROK 6, TAUGTOG, BOTOLAN, ZAMBALES', 'REISS_15_20240713023617.png', 0);
 
 -- --------------------------------------------------------
 
@@ -46,13 +79,6 @@ CREATE TABLE `employee_tbl` (
   `profile` varchar(255) NOT NULL,
   `deleted` tinyint(1) NOT NULL DEFAULT 0 COMMENT '1 = deleted, 0 = not deleted'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `employee_tbl`
---
-
-INSERT INTO `employee_tbl` (`employee_id`, `user_id`, `first_name`, `middle_name`, `last_name`, `suffix_name`, `birthdate`, `age`, `sex`, `contact_no`, `personal_address`, `occupation`, `emergency_contact_name`, `emergency_contact_no`, `emergency_contact_address`, `profile`, `deleted`) VALUES
-(3, 15, 'FRIEDA', 'TREVOR', 'REISS', 'NA', '2001-05-09', 23, 'FEMALE', '09111111111', 'ILANG ILANG STREET', 'STAFF', 'RALPH CUSTODIO', '09222222222', 'PUROK 6, TAUGTOG, BOTOLAN, ZAMBALES', 'REISS_15_20240713023617.png', 0);
 
 -- --------------------------------------------------------
 
@@ -86,7 +112,7 @@ CREATE TABLE `student_tbl` (
 --
 
 INSERT INTO `student_tbl` (`student_id`, `user_id`, `first_name`, `middle_name`, `last_name`, `suffix_name`, `birthdate`, `age`, `sex`, `contact_no`, `personal_address`, `course`, `year_level`, `emergency_contact_name`, `emergency_contact_no`, `emergency_contact_address`, `profile`, `deleted`) VALUES
-(3, 14, 'RALPH', 'DOCUTIN', 'CUSTODIO', 'II', '2001-09-05', 22, 'MALE', '09123456789', 'PUROK 6, TAUGTOG, BOTOLAN, ZAMBALES', 'BSIT', 3, 'FRIEDA REISS', '09123456789', 'ILANG ILANG STREET', 'CUSTODIO_14_20240713023209.png', 0);
+(3, 14, 'RALPH', 'DOCUTIN', 'CUSTODIO', 'II', '2001-05-09', 22, 'MALE', '09123456789', 'PUROK 6, TAUGTOG, BOTOLAN, ZAMBALES', 'BSIT', 3, 'FRIEDA REISS', '09123456789', 'ILANG ILANG STREET', 'CUSTODIO_14_20240713023209.png', 0);
 
 -- --------------------------------------------------------
 
@@ -109,12 +135,18 @@ CREATE TABLE `user_tbl` (
 --
 
 INSERT INTO `user_tbl` (`user_id`, `no`, `email`, `password`, `role`, `status`, `deleted`) VALUES
-(14, '20-00237', 'ralphcustodio@proton.me', '$2y$10$.FOqNGlceW0fxcGgr2K9EuWeAKkbh6jidapvpu2T8K0CQibaoZ1qe', 'STUDENT', 'PENDING', 0),
-(15, '20-00238', 'friedareiss@proton.me', '$2y$10$SaHjeR0XMwNrCoKfKC0E0e/krZcqiaInVi7SwlsTXZocJ5vp3WjJi', 'EMPLOYEE', 'PENDING', 0);
+(14, '20-00237', 'ralphcustodio@proton.me', '$2y$10$.FOqNGlceW0fxcGgr2K9EuWeAKkbh6jidapvpu2T8K0CQibaoZ1qe', 'STUDENT', 'APPROVED', 0),
+(15, '20-00238', 'friedareiss@proton.me', '$2y$10$SaHjeR0XMwNrCoKfKC0E0e/krZcqiaInVi7SwlsTXZocJ5vp3WjJi', 'ADMIN', 'APPROVED', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin_tbl`
+--
+ALTER TABLE `admin_tbl`
+  ADD PRIMARY KEY (`admin_id`);
 
 --
 -- Indexes for table `employee_tbl`
@@ -139,6 +171,12 @@ ALTER TABLE `user_tbl`
 --
 
 --
+-- AUTO_INCREMENT for table `admin_tbl`
+--
+ALTER TABLE `admin_tbl`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `employee_tbl`
 --
 ALTER TABLE `employee_tbl`
@@ -148,7 +186,7 @@ ALTER TABLE `employee_tbl`
 -- AUTO_INCREMENT for table `student_tbl`
 --
 ALTER TABLE `student_tbl`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user_tbl`
