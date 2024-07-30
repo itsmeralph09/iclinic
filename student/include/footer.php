daad3a31e909c3754802eaa848511002c8904b50
itsmeralph09 <ralphenigmatic@gmail.com>
2024-07-30 18:33:29 +0800

    Added base for students dashboard

---
+++ student/include/footer.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+            <footer class="sticky-footer text-white" style="background-color: #303991;">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; iClinic - PRMSU Candelaria 2024</span>
+                    </div>
+                </div>
+            </footer>
+<!-- End of Footer -->
