1c6d5a28931a739dd35e0d3314b416d39a9cc84e
itsmeralph09 <ralphenigmatic@gmail.com>
2024-10-13 07:18:58 +0800

    Added profile name

--- admin/include/topbar.php
+++ admin/include/topbar.php
@@ -37,6 +37,16 @@
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="">
+                                    <?php
+                                        if (isset($_SESSION['fullname'])) {
+                                            echo $_SESSION['fullname'];
+                                        }else{
+                                            echo "Guest User";
+                                        }
+                                    ?>
+                                </a>
+                                <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
--- employee/include/topbar.php
+++ employee/include/topbar.php
@@ -37,17 +37,19 @@
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
+                                <a class="dropdown-item" href="">
+                                    <?php
+                                        if (isset($_SESSION['fullname'])) {
+                                            echo $_SESSION['fullname'];
+                                        }else{
+                                            echo "Guest User";
+                                        }
+                                    ?>
+                                </a>
+                                <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
                                 </a>
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
