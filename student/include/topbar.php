0b9b521632fe0d6ca205a6f7c2cde87ee473f553
itsmeralph09 <ralphenigmatic@gmail.com>
2024-10-14 21:45:25 +0800

    Added profile info page

--- student/include/topbar.php
+++ student/include/topbar.php
@@ -47,7 +47,7 @@
                                     ?>
                                 </a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="profile.php">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
